93a7c49aa7aa4e24c7bc3c2d54792b0cb36286eb
Kritika Agrawal <55151015+agrawal-kritika@users.noreply.github.com>
2025-02-07 23:20:33 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//Simple Java Program that prints "Hello World"
+class HelloWorld {
+    // Every JAVA program has main method
+    // Program execution begins with the main method
+    public static void main(String[] args) {
+        System.out.println("Hello World, happy programming!"); // Print the string on standard output
+    }
+}
