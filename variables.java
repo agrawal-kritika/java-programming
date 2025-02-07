0b65c19936d62f416663c5d95d9e6d894aec86da
Kritika Agrawal <55151015+agrawal-kritika@users.noreply.github.com>
2025-02-07 23:38:53 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,20 @@
+/* Data types, variables and its naming conventions */
+
+class variables {
+    public static void main(String[] args) {
+        String name = "Monica";
+        int age = 24;
+        char gender = 'F';
+        long phno = 1234567890l; // long literals should be suffixed with L
+        float marks = 99.8f; // float literals should be suffixed with f
+        double salary = 80000.234567;
+
+        // printing the values
+        System.out.println("Name: " + name); // To print the value, it should be given with + symbol
+        System.out.println("age: " + age);
+        System.out.println("Gender: " + gender);
+        System.out.println("Phone no.: " + phno);
+        System.out.println("marks: " + marks);
+        System.out.println("Salary: " + salary);
+    }
+}
