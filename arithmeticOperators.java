6bf2d5d1075e7705d13fb179c6620b5a8080eb9e
Kritika Agrawal <55151015+agrawal-kritika@users.noreply.github.com>
2025-02-13 16:12:14 +0530

    arithmeticOperators.java

--- arithmeticOperators.java
+++ arithmeticOperators.java
@@ -5,14 +5,17 @@
 
 class arithmeticOperators {
     public static void main(String[] args) {
-        float number1 = 50.79f;
-        int number2 = 15;
-        float sum, diff, mul, div, rem; // creating variables to store the values
+	// Declaring and initializing variables
+        float number1 = 50.79f;  // Floating-point number
+        int number2 = 15; 	 // Integer number
+        float sum, diff, mul, div, rem; // Creating variables to store the results of arithmetic operations
+	// Performing arithmetic operations
         sum = number1 + number2;
         diff = number1 - number2;
         mul = number1 * number2;
         div = number1 / number2;
         rem = number1 % number2;
+	// Printing the results
         System.out.println("Sum: " + sum);
         System.out.println("Difference: " + diff);
         System.out.println("Multiplication: " + mul);
